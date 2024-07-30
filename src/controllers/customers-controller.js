248033e946793748e75a14fbc9c496d0749fa4d2
truds99 <dudscardoso9@gmail.com>
2024-07-30 01:36:43 -0300

    add post customer controller

---
+++ src/controllers/customers-controller.js
@@ -0,0 +1,22 @@
+import db from "../config/database.js";
+import httpStatus from "http-status";
+
+export async function postCustomer(req, res) {
+    const { name, cpf, phone } = req.body;
+
+    try {
+        const { rowCount } = await db.query(`
+            SELECT * FROM customers
+            WHERE cpf = $1;
+        `, [cpf]);
+        if (rowCount > 0) return res.sendStatus(httpStatus.CONFLICT)
+        
+        await db.query(`
+            INSERT INTO customers (name, cpf, phone)
+            VALUES ($1, $2, $3);
+        `, [name, cpf, phone])
+        res.sendStatus(httpStatus.CREATED);
+    } catch (error) {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import db from "./config/database.js";
 import { gamesRouter } from "./routers/games-router.js";
+import { customersRouter } from "./routers/customers-router.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(json());
 
 app.use(gamesRouter);
+app.use(customersRouter)
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
