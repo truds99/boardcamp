8bf25ea9d3c895edea6888de9782a7bd72609116
truds99 <dudscardoso9@gmail.com>
2024-07-29 22:40:46 -0300

    add db connection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import connection from "./config/database.js";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(json());
-
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
