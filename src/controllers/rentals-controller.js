60a645be1412752fe49d0a6eb7bd702717752fc5
truds99 <dudscardoso9@gmail.com>
2024-07-31 19:43:35 -0300

    add post rental service

--- src/controllers/rentals-controller.js
+++ src/controllers/rentals-controller.js
@@ -1,45 +1,13 @@
 import db from "../config/database.js";
 import httpStatus from "http-status";
 import dayjs from "dayjs";
+import { postRentalService } from "../services/rentals-services.js";
 
 export async function postRental(req, res) {
-    const { customerId, gameId, daysRented } = req.body;
-    const rentDate = dayjs().format('YYYY-MM-DD');
-    const returnDate = null, delayFee = null;
-    
     try {
-        const game = await db.query(`
-            SELECT * FROM games
-            WHERE id = $1;
-        `, [gameId]);
-        if (!game.rows.length) return res.sendStatus(httpStatus.NOT_FOUND);
-
-        const customer = await db.query(`
-            SELECT * FROM customers
-            WHERE id = $1;
-        `, [customerId]);
-        if (!customer.rows.length) return res.sendStatus(httpStatus.NOT_FOUND);
-
-        const pricePerDay = game.rows[0].pricePerDay;
-        const originalPrice = pricePerDay * daysRented;
-        const stock = game.rows[0].stockTotal;
-
-        const pastRentals = await db.query(`
-            SELECT * FROM rentals
-            WHERE "gameId" = $1 
-            AND "returnDate" IS NULL;
-        `, [gameId]);
-        const openRentals = pastRentals.rowCount;
-        if (openRentals >= stock) return res.sendStatus(httpStatus.UNPROCESSABLE_ENTITY);
-
-        await db.query(`
-            INSERT INTO rentals
-            ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee")
-            VALUES ($1, $2, $3, $4, $5, $6, $7)
-        `, [customerId, gameId, daysRented, rentDate, originalPrice, returnDate, delayFee]);
-
-        res.sendStatus(httpStatus.CREATED);
-        
+        const result = await postRentalService(req.body);
+        if (result) return res.sendStatus(httpStatus.CREATED);
+        res.sendStatus(400);
     } catch (error) {
         res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
     }
@@ -103,7 +71,7 @@ export async function endRental(req, res) {
         if(!rental.rows.length) return res.sendStatus(httpStatus.NOT_FOUND);
 
         rental = rental.rows[0];
-       if(rental.returnDate) return res.sendStatus(httpStatus.UNPROCESSABLE_ENTITY);
+        if(rental.returnDate) return res.sendStatus(httpStatus.UNPROCESSABLE_ENTITY);
         
         const returnDate =  dayjs().format('YYYY-MM-DD');
         const scheduledDate = dayjs(rental.rentDate).add(rental.daysRented, 'day');
---
+++ src/services/rentals-services.js
@@ -0,0 +1,39 @@
+import dayjs from "dayjs";
+import db from "../config/database.js";
+
+export async function postRentalService({ customerId, gameId, daysRented }) {
+    const rentDate = dayjs().format('YYYY-MM-DD');
+    const returnDate = null, delayFee = null;
+    
+        const game = await db.query(`
+            SELECT * FROM games
+            WHERE id = $1;
+        `, [gameId]);
+        if (!game.rows.length) return null;
+
+        const customer = await db.query(`
+            SELECT * FROM customers
+            WHERE id = $1;
+        `, [customerId]);
+        if (!customer.rows.length) return null;
+
+        const pricePerDay = game.rows[0].pricePerDay;
+        const originalPrice = pricePerDay * daysRented;
+        const stock = game.rows[0].stockTotal;
+
+        const pastRentals = await db.query(`
+            SELECT * FROM rentals
+            WHERE "gameId" = $1 
+            AND "returnDate" IS NULL;
+        `, [gameId]);
+        const openRentals = pastRentals.rowCount;
+        if (openRentals >= stock) return null;
+
+        await db.query(`
+            INSERT INTO rentals
+            ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee")
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+        `, [customerId, gameId, daysRented, rentDate, originalPrice, returnDate, delayFee]);
+
+        return true;
+}
