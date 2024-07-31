243e777af5c580665db98b673c3b43c2b1c75f07
truds99 <dudscardoso9@gmail.com>
2024-07-31 04:03:49 -0300

    add delete rental controller

--- src/controllers/rentals-controller.js
+++ src/controllers/rentals-controller.js
@@ -124,3 +124,26 @@ export async function endRental(req, res) {
      }
 }
 
+export async function deleteRental(req, res) {
+    const id = Number(req.params.id);
+    if(isNaN(id) || id % 1 !== 0 || id <= 0) return res.sendStatus(httpStatus.BAD_REQUEST);
+    
+     try {
+        let rental = await db.query(`
+            SELECT * FROM rentals
+            WHERE id = $1;
+        `, [id])
+        if(!rental.rows.length) return res.sendStatus(httpStatus.NOT_FOUND);
+        if(!rental.rows[0].returnDate) return res.sendStatus(httpStatus.BAD_REQUEST);
+
+        await db.query(`
+            DELETE FROM rentals
+            WHERE id = $1;
+        `, [id]);
+
+        res.sendStatus(httpStatus.OK);
+     } catch (error) {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+     }
+}
+
--- src/routers/rentals-router.js
+++ src/routers/rentals-router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import validateSchema from '../middlewares/schemas-middleware.js';
-import { postRental, getRentals, endRental } from "../controllers/rentals-controller.js";
+import { postRental, getRentals, endRental, deleteRental } from "../controllers/rentals-controller.js";
 import { rentalSchema } from "../schemas/rental-schema.js";
 
 export const rentalsRouter = Router();
@@ -8,3 +8,4 @@ export const rentalsRouter = Router();
 rentalsRouter.post('/rentals', validateSchema(rentalSchema), postRental);
 rentalsRouter.get('/rentals', getRentals);
 rentalsRouter.post('/rentals/:id/return', endRental);
+rentalsRouter.delete('/rentals/:id', deleteRental);
