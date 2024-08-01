ecfe6a2794b3fb0acec9140dc0c1a865c0b308c0
truds99 <dudscardoso9@gmail.com>
2024-08-01 02:59:32 -0300

    add throw to customers

--- src/controllers/customers-controller.js
+++ src/controllers/customers-controller.js
@@ -7,21 +7,20 @@ import {
 
 export async function postCustomer(req, res) {
     try {
-        const result = await postCustomerService(req.body);
-        if (result) return res.sendStatus(httpStatus.OK);
-        res.sendStatus(400);
-    } catch (error) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+        await postCustomerService(req.body);
+        res.sendStatus(httpStatus.CREATED);
+    } catch (e) {
+        if (e.type === 'conflict') return res.status(httpStatus.CONFLICT).send(e.message);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e.message);
     }
 }
 
 export async function getCustomers(req, res) {
     try {    
         const customers = await getCustomersService();
-        if (customers) return res.send(customers).status(200);
-        res.sendStatus(400);
-    } catch (error) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+        res.send(customers).status(httpStatus.OK);
+    } catch (e) {
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e.message);
     }
 }
 
@@ -29,9 +28,9 @@ export async function getOneCustomer(req, res) {
     const id = Number(req.params.id);
     try {    
         const customer = await getOneCustomerService(id);
-        if (customer) return res.send(customer).status(200);
-        res.sendStatus(400);
-    } catch (error) {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+        res.send(customer).status(httpStatus.OK);
+    } catch (e) {
+        if (e.type === 'not_found') return res.status(httpStatus.NOT_FOUND).send(e.message);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e.message);
     }
 }
--- src/services/customers-services.js
+++ src/services/customers-services.js
@@ -3,21 +3,25 @@ import { getCustomerByCpfRep, getCustomerByIdRep, getCustomersRep, insertCustome
 
 export async function postCustomerService({ name, cpf, phone }) {
     const { rowCount } = await getCustomerByCpfRep(cpf);
-    if (rowCount > 0) return null;
+    if (rowCount > 0) throw {
+        type: 'conflict',
+        message: 'this cpf is already in use'
+    }
     
     await insertCustomerRep(name, cpf, phone);
-
-    return true;
 }
 
 export async function getCustomersService() {
     const customers = await getCustomersRep();
     return customers.rows;
-}
+} 
 
 export async function getOneCustomerService(id){
     if (isNaN(id) || id%1 !== 0 || id <= 0) return null; 
     const customer = await getCustomerByIdRep(id);
-    if (!customer.rows.length) return null;
+    if (!customer.rows.length) throw {
+        type: 'not_found',
+        message: 'customer not found'
+    }
     return customer.rows;
 }
